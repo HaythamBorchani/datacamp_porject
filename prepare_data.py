e9163bd7dfc5343fc7f9f7fc8350b88bc40359d6
HaythamBorchani <haytham.borchani@ensta-paris.fr>
2024-03-02 22:34:34 +0100

    add workflow

---
+++ prepare_data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+
+
+# 1 - read in or download the data.
+df = pd.read_csv(os.path.join('data','dataset.csv'))
+
+# 2 - Perform any data cleaning and split into private train/test subsets,
+# if required. Neither steps required in this case.
+
+# 3 - Split Public/Private data.
+df_public, df_private = train_test_split(df, test_size=0.7, random_state=57)
+    # specify the random_state to ensure reproducibility
+
+# 4 - Split public train/test subsets. 
+
+df_public_train, df_public_test = train_test_split(
+    df_public, test_size=0.2, random_state=57)
+    # specify the random_state to ensure reproducibility
+
+# Define the directory path
+public_path = 'data\\public'
+
+# Check if the directory exists, if not, create it
+if not os.path.exists(public_path):
+    os.makedirs(public_path)
+
+df_public_train.to_csv(os.path.join(public_path, 'train.csv'), index=False)
+df_public_test.to_csv(os.path.join(public_path, 'test.csv'), index=False)
+
+
+# 5 - Split private train/test subsets.
+
+# Define the directory path
+private_path = 'data'
+
+# Check if the directory exists, if not, create it
+if not os.path.exists(private_path):
+    os.makedirs(private_path)
+
+df_private_train, df_private_test = train_test_split(
+    df_private, test_size=0.2, random_state=57)
+    # specify the random_state to ensure reproducibility
+df_private_train.to_csv(os.path.join(private_path, 'train.csv'), index=False)
+df_private_test.to_csv(os.path.join(private_path, 'test.csv'), index=False)
---
+++ problem.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+import rampwf as rw
+from sklearn.model_selection import StratifiedShuffleSplit
+
+problem_title = 'Churn prediction'
+_target_column_name = 'churn'
+_ignore_column_names = ['id_client']
+_prediction_label_names = ["oui", "non"]
+
+cat_to_int = {'oui': 1, 'non': 0}
+int_to_cat = {1: 'oui', 0: 'non'}
+
+_prediction_label_int = [cat_to_int[cat] for cat in _prediction_label_names]
+
+# A type (class) which will be used to create wrapper objects for y_pred
+Predictions = rw.prediction_types.make_multiclass(
+    label_names=_prediction_label_int)
+# An object implementing the workflow
+workflow = rw.workflows.Classifier()
+
+score_types = [
+    rw.score_types.BalancedAccuracy(name='bal_acc', precision=3, adjusted=False),
+    rw.score_types.ROCAUC(name='auc', precision=3),
+    rw.score_types.Accuracy(name='acc', precision=3),
+    rw.score_types.NegativeLogLikelihood(name='nll'),
+]
+
+
+def get_cv(X, y):
+    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
+    return cv.split(X, y)
+
+
+def _read_data(path, f_name):
+    data = pd.read_csv(os.path.join(path, 'data', f_name),sep=';')
+    y_array = data[_target_column_name].map(cat_to_int).fillna(-1).astype("int8").values
+    X_df = data.drop([_target_column_name] + _ignore_column_names, axis=1)
+    return X_df, y_array
+
+
+def get_train_data(path='.\\data\\public'):
+    f_name = 'train.csv'
+    return _read_data(path, f_name)
+
+
+def get_test_data(path='.\\data\\public'):
+    f_name = 'test.csv'
+    return _read_data(path, f_name)
