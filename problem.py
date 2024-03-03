75c5b85b5fb98810cc601de07cbaa387ba933268
Dahouathi <wadhah.dahwethi@gmail.com>
2024-03-03 23:38:57 +0100

    [problem.py] added ignored columns

--- problem.py
+++ problem.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 problem_title = 'Selectivity of higher education programs in France'
 _target_column_name = 'selectivity_category'
-_ignore_column_names = []
+_ignore_column_names = ['prop_tot_bt_brs', 'pct_tbf', 'pct_b', 'pct_acc_debutpp', 'nb_voe_pp_at', 'nb_voe_pc_bp', 'pct_acc_datebac', 'acc_tbf', 'pct_bours', 'pct_acc_finpp', 'nb_voe_pc_bt', 'pct_mention_nonrenseignee', 'pct_ab', 'acc_tb', 'nb_voe_pp', 'pct_aca_orig', 'voe_tot', 'pct_aca_orig_idf', 'voe_tot_f', 'acc_mention_nonrenseignee', 'pct_f']
 _prediction_label_names = ["Très sélective", "Peu sélective", "Non sélective"]
 
 cat_to_int = {'Très sélective': 2, 'Peu sélective': 1, 'Non sélective': 0}
@@ -21,13 +21,12 @@ workflow = rw.workflows.Classifier()
 
 score_types = [
     rw.score_types.BalancedAccuracy(name='bal_acc', precision=3, adjusted=False),
-    rw.score_types.ROCAUC(name='auc', precision=3),
     rw.score_types.Accuracy(name='acc', precision=3),
 ]
 
 
 def get_cv(X, y):
-    cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=57)
+    cv = StratifiedShuffleSplit(n_splits=2, test_size=0.2, random_state=57)
     return cv.split(X, y)
 
 
